1a5df2ff549f3aa8ddf33085e49a287770f7b626
kisevrotfl <lionkis13@gmail.com>
2024-03-13 16:33:58 +0200

    get logo from magento and change it on venia

---
+++ src/components/talons/logo.gql.js
@@ -0,0 +1,14 @@
+import {gql} from "@apollo/client";
+
+export const LET_STORE_LOGO = gql `
+    query getStoryLogo{
+        storeConfig{
+            id
+            header_logo_src
+            logo_alt
+            logo_height
+            logo_width
+            store_code
+        }
+    }
+`;
--- src/targets/local-intercept.js
+++ src/targets/local-intercept.js
@@ -3,11 +3,25 @@ const moduleOverridePlugin = require('../moduleOverrideWebpackPlugin');
 const componentOverrideProductFullDetail = module.exports = {
     ['@magento/venia-ui/lib/components/ProductFullDetail']:'src/components/ProductFullDetail'
 };
+const componentLogo = module.exports = {
+    ['@magento/venia-ui/lib/components/Logo']:'src/components/Logo'
+};
+const componentOverRiddingLogo = module.exports = {
+    ['@magento/peregrine/lib/util/makeUrl.js']:'src/components/talons/makeUrl.js'
+};
 
 module.exports = targets => {
 
     targets.of('@magento/pwa-buildpack').webpackCompiler.tap(compiler => {
         new moduleOverridePlugin(componentOverrideProductFullDetail).apply(compiler);
+    });
+
+    targets.of('@magento/pwa-buildpack').webpackCompiler.tap(compiler => {
+        new moduleOverridePlugin(componentLogo).apply(compiler);
+    });
+
+    targets.of('@magento/pwa-buildpack').webpackCompiler.tap(compiler => {
+        new moduleOverridePlugin(componentOverRiddingLogo).apply(compiler);
     });
 
     targets.of("@magento/venia-ui").routes.tap(routes => {
